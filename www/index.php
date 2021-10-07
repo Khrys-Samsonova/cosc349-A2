bdc872b69ab6c871523e018afd8bc1803bfbd055
Oliver <crool215@otago.student.ac.z>
2021-10-07 22:35:32 +1300

    can delete from db, trying to push deleted rows to bucket

--- www/index.php
+++ www/index.php
@@ -28,32 +28,33 @@
         <input type="text" name="meal_description" maxlength="256" size="60" />
       </td>
       <td>
-        <input type="submit" value="Add Meal" />
+        <input type="submit" value="Add Meal" name="submit" />
       </td>
     </tr>
   </table>
 </form>
 <?php
 if(isset($_POST["submit"])){
-    $hostname = 'assignment-2-rds.cdryxhulhnxp.us-east-1.rds.amazonaws.com';
-    $username = 'admin';
-    $password = 'khrysoliver';
 
-    try{
-        $dbh = new PDO("mysql:host=$hostname, dbname=assignment2Db", $username, $password);
 
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO meals (meal_name, meal_description) VALUES ('".$_POST["meal_name"]."','".$_POST["meal_description"]."')";
+    $db_host = 'assignment-2-rds.cdryxhulhnxp.us-east-1.rds.amazonaws.com';
+    $db_name = 'assignment2Db';
+    $db_user = 'admin';
+    $db_passwd = 'khrysoliver';
 
-        $dbh = null;
-    }
-    catch(PDOException $e){
-        echo $e->getMessage();
-    }
+    $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+    $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+    $m_name = $_POST["meal_name"];
+    $m_desc = $_POST["meal_description"];
+
+    $q_m = $pdo->query("INSERT INTO meals(meal_name, meal_description) VALUES ('$m_name', '$m_desc')");
 
 }
 ?>
 
+<form action='' method='post'>
 <table class="center" border="1">
 <tr><th>Meal Name</th><th>Meal Description</th></tr>
 
@@ -72,11 +73,39 @@ $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 $q_m = $pdo->query("SELECT * FROM meals");
 
 
+
 while($row = $q_m->fetch()) {
-  echo "<tr><td>".$row["meal_name"]."</td><td>".$row["meal_description"]."</td></tr>";
+
+  echo "<tr>";
+  echo "<td>".$row["meal_name"]."</td>";
+  echo "<td>".$row["meal_description"]."</td>";
+  echo "<td><button type='submit' name='deleteItem' id='deleteItem' value='".$row["meal_name"]."'/> Delete </buttont></td>";
+  echo "</tr>";
+
 }
+
+if(isset($_POST["deleteItem"])){
+
+    $delete = $_POST['deleteItem'];
+
+    $savedVar = $pdo->query("SELECT * FROM meals WHERE meal_name = '$delete'");
+
+    $archivedVar = fopen('/var/www/html/archives.csv', 'w');
+
+    fwrite($archivedVar, savedVar);
+
+    /* echo shell_exec ("aws s3 mv archives.csv s3://assignment2-archive-bucket"); */
+
+    fclose($archivedVar);
+
+    $q_d = $pdo->query("DELETE FROM meals WHERE meal_name = '$delete'");
+}
+
+
+
+
 ?>
 </table>
+</form>
 </body>
 </html>
-
